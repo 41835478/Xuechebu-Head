ff7bab4758e9eb29e8e13ee7969192bbe6c7fd70
GitQinling <qinlingzhao@sina.cn>
2017-12-12 17:53:32 +0800

    学员统计接口调试

--- pages/stuDetail/student.js
+++ pages/stuDetail/student.js
@@ -1,40 +1,45 @@
 var Zan = require('../../dist/index');
 var wxCharts = require('../../utils/wxcharts.js');
 var lineChart = null;
-var seletedTab = '';
+var seletedTab = '1'; //查询学生类型1表示报名学员 2表示退学学员  3表示毕业学员
+var dateTab = '1'; //1表示七天的 2表示一个月 3表示三个月 4表示本年度
 Page(Object.assign({}, Zan.Tab, {
   data: {
     tab1: {
       list: [{
-        id: 'all',
+        id: '1',
         title: '报名人数'
       }, {
-        id: 'topay',
+        id: '2',
         title: '退学人数'
       }, {
-        id: 'tosend',
+        id: '3',
         title: '毕业人数'
       }],
-      selectedId: 'all',
+      selectedId: '1',
       scroll: false
     },
     show:'film_favorite',
     dateArray: [
       {
         date: '近七天',
-        changeColor: 'selected'
+        changeColor: 'selected',
+        id:'1'
       },
       {
         date: '近一个月',
-        changeColor: 'normal'
+        changeColor: 'normal',
+        id:'2'
       },
       {
         date: '近三个月',
-        changeColor: 'normal'
+        changeColor: 'normal',
+        id:'3'
       },
       {
         date: '本年度',
-        changeColor: 'normal'
+        changeColor: 'normal',
+        id:'4'
       }
     ],
     classifyArray:[
@@ -44,6 +49,7 @@ Page(Object.assign({}, Zan.Tab, {
     ]
   },
 
+//选择分类事件
   handleZanTabChange(e) {
     var componentId = e.componentId;
     var selectedId = e.selectedId;
@@ -54,11 +60,13 @@ Page(Object.assign({}, Zan.Tab, {
     });
   },
   
+//选择日期事件
   setDate(e) {
     var that = this;
     var txtArray = [];
     for (var i = 0; i < that.data.dateArray.length; i++) {
       if (e.currentTarget.dataset.date == that.data.dateArray[i].date) {
+        dateTab = that.data.dateArray[i].id;//记录选择的日期
         txtArray.push({date:that.data.dateArray[i].date, changeColor:'selected'});
       } else {
         txtArray.push({ date: that.data.dateArray[i].date, changeColor: 'normal' });
@@ -77,6 +85,9 @@ Page(Object.assign({}, Zan.Tab, {
     } catch (e) {
       console.error('getSystemInfoSync failed!');
     }
+
+    this.loadData();
+    //请求报名人数数据
 
     var simulationData = this.createSimulationData();
     lineChart = new wxCharts({
@@ -130,6 +141,25 @@ Page(Object.assign({}, Zan.Tab, {
       categories: categories,
       data: data
     }
+  },
+
+  loadData:function() {
+    var that = this;
+    wx.request({
+      url: 'https://xzzstest1.xuechebu.com/SchoolMaster/statisticsdata/getSchoolStatisticsData',
+      method: 'GET',
+      data: {
+        jgid: '14001',
+        datetype : dateTab,
+        studenttype : seletedTab
+      },
+      header: {
+        "Content-Type": "json",
+      },
+      success: function (res) {
+        that.setData({ dataSource: res.data })
+      }
+    })
   }
 
 }));
